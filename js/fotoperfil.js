74fc8ed6463f7f12c9b61a6ce5e68b3cbf079cb9
Ana Julia <167318220+Bressiano@users.noreply.github.com>
2025-03-18 07:19:04 -0300

    atualização 18/03/2025

--- js/fotoperfil.js
+++ js/fotoperfil.js
@@ -1,6 +1,41 @@
-function escolherPersonagem(imgSrc) {
-    localStorage.setItem('perfilImg', imgSrc);
-    atualizarPerfil();
+function openMenu() {
+  const menu = document.getElementById('menu');
+  menu.style.display = 'flex'; // Mostra o menu em tela cheia
+}
+
+function closeMenu() {
+  const menu = document.getElementById('menu');
+  menu.style.display = 'none'; // Oculta o menu
+}
+const accessButton = document.getElementById("accessButton");
+const accessCard = document.getElementById("accessCard");
+
+accessButton.addEventListener("click", () => {
+    accessCard.classList.toggle("hidden");
+});
+
+function zoomIn() {
+    document.body.style.zoom = (parseFloat(document.body.style.zoom || 1) + 0.1).toString();
+}
+
+function zoomOut() {
+    document.body.style.zoom = Math.max(parseFloat(document.body.style.zoom || 1) - 0.1, 0.5).toString();
+}
+
+function increaseFont() {
+    document.body.style.fontSize = (parseFloat(getComputedStyle(document.body).fontSize) + 2) + "px";
+}
+
+function decreaseFont() {
+    document.body.style.fontSize = Math.max(parseFloat(getComputedStyle(document.body).fontSize) - 2, 10) + "px";
+}
+
+function toggleDaltonicMode() {
+    document.body.classList.toggle("daltonic-mode");
+}
+
+function toggleLightDarkMode() {
+    document.body.classList.toggle("dark-mode");
 }
 
 function atualizarPerfil() {
--- js/profissionais.js
+++ js/profissionais.js
@@ -1,3 +1,4 @@
+// const
 const profissionaisData = {
     "Taquaritinga": [
         {
